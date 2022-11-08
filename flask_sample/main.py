8592946ef13ac07e61a220c2f2e28569c890edec
“Satyam <sp2943@njit.edu>
2022-11-07 22:19:56 -0500

    flask1

--- flask_sample/main.py
+++ flask_sample/main.py
@@ -5,21 +5,23 @@ from flask import Flask
 
 # added so modules can be found between the two different lookup states:
 # from tests and from regular running of the app
-CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+CURR_DIR = os.path.dirname(os.path.abspath(_file_))
 print(CURR_DIR)
 sys.path.append(CURR_DIR)
 
 
-# app = Flask(__name__)
+# app = Flask(_name_)
 def create_app(config_filename=''):
-    app = Flask(__name__)
+    app = Flask(_name_)
     # app.config.from_pyfile(config_filename)
     with app.app_context():
         from views.hello import hello
         app.register_blueprint(hello)
+        from views.sample import sample
+        app.register_blueprint(sample)
         return app
 
 
 app = create_app()
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
