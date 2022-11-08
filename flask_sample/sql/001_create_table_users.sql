3fd27207ab6c1395f4e91cc9a7c7872f8b3dfb7e
“Satyam <sp2943@njit.edu>
2022-11-08 00:58:25 -0500

    flask auth

--- flask_sample/sql/001_create_table_users.sql
+++ flask_sample/sql/001_create_table_users.sql
@@ -1,9 +1,8 @@
--- Active: 1663209307387@@db.ethereallab.app@3306@sp2943
 CREATE TABLE
-    IS601_Sample(
+    IS601_Users(
         id int auto_increment PRIMARY KEY,
-        name VARCHAR(20) unique,
-        val VARCHAR(40),
+        email VARCHAR(60) unique,
+        password VARCHAR(60) not null,
         created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )
